657322d35e05316b58632cc4f1d8bd5818251c80
Kevin <kevin_mendez30@outlook.com>
2021-10-29 12:39:43 -0600

    refactor: 💡 (index.ts) for export

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { base64SvgToBase64Png, svgToSvgBase64, svgToPng } from './svg-convert'
+import { downloadSVG, downloadPNG } from './svg-download'
+
+export { base64SvgToBase64Png, svgToSvgBase64, svgToPng, downloadSVG, downloadPNG }
